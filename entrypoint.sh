ab83e3de4d9e23a1de45d0c7d97d953544e602cc
Boris Breuer <mail@borisbreuer.de>
2023-04-02 19:59:13 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sshpass -p $INPUT_PASSWORD ssh "$INPUT_USERNAME"@"$INPUT_HOST" -t "rm -r test/"
+sshpass -p $INPUT_PASSWORD ssh "$INPUT_USERNAME"@"$INPUT_HOST" -tt "rm -r test/"
 
 echo '"$INPUT_TARGET"'
 
