b307cd81c5209f91c95ff8820b4ab901f7340e0e
Boris Breuer <mail@borisbreuer.de>
2023-04-02 19:47:10 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 sshpass -p $INPUT_PASSWORD ssh "$INPUT_USERNAME"@"$INPUT_HOST" 'rm -rf test'
 
+echo "$INPUT_TARGET"
+
 #sshpass -p $INPUT_PASSWORD scp -qr -P $INPUT_PORT -o StrictHostKeyChecking=no $INPUT_SOURCE "$INPUT_USERNAME"@"$INPUT_HOST":"$INPUT_TARGET"
 
 #echo "SCP-Deployer - Start"
